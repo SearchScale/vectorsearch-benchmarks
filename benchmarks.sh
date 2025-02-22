8f775626ede1d6f429b2564a0f44c5d3be873809
ChrisHegarty <chegar999@gmail.com>
2025-02-22 14:05:41 +0000

    enable the panama vector api in benchmark runs

--- benchmarks.sh
+++ benchmarks.sh
@@ -1,3 +1,10 @@
 mvn clean package
 
-java -cp $HOME/.m2/repository/com/nvidia/cuvs/cuvs-java/25.02.0/cuvs-java-25.02.0.jar:target/vectorsearch-benchmarks-1.0-jar-with-dependencies.jar com.searchscale.lucene.cuvs.benchmarks.LuceneCuvsBenchmarks jobs.json
+CUVS_JAVA_JAR=$HOME/.m2/repository/com/nvidia/cuvs/cuvs-java/25.02.0/cuvs-java-25.02.0.jar
+BENCHMARK_JAR=target/vectorsearch-benchmarks-1.0-jar-with-dependencies.jar
+
+"$JAVA_HOME"/bin/java -cp "$CUVS_JAVA_JAR":"$BENCHMARK_JAR" \
+--add-modules=jdk.incubator.vector \
+--enable-native-access=ALL-UNNAMED \
+com.searchscale.lucene.cuvs.benchmarks.LuceneCuvsBenchmarks \
+jobs.json
