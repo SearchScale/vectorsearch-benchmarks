bfea233c3615db1ae19cff24642c3af46e34bbd1
ChrisHegarty <chegar999@gmail.com>
2025-02-26 10:23:44 +0000

    remove duplicate --add-modules jdk.incubator.vector

--- benchmarks.sh
+++ benchmarks.sh
@@ -4,7 +4,7 @@ CUVS_VERSION=25.02.0
 CUVS_JAVA_JAR=$HOME/.m2/repository/com/nvidia/cuvs/cuvs-java/$CUVS_VERSION/cuvs-java-$CUVS_VERSION.jar
 BENCHMARK_JAR=target/vectorsearch-benchmarks-1.0-jar-with-dependencies.jar
 
-"$JAVA_HOME"/bin/java --add-modules jdk.incubator.vector -cp "$CUVS_JAVA_JAR":"$BENCHMARK_JAR" \
+"$JAVA_HOME"/bin/java -cp "$CUVS_JAVA_JAR":"$BENCHMARK_JAR" \
 --add-modules=jdk.incubator.vector \
 --enable-native-access=ALL-UNNAMED \
 com.searchscale.lucene.cuvs.benchmarks.LuceneCuvsBenchmarks \
