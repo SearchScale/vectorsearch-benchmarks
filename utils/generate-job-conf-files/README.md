519a0ea54afca1748bcacf773972efe31480b061
Vivek Narang <vivek@searchscale.com>
2025-03-02 14:16:34 -0500

    Add job configuration files generator utility

---
+++ utils/generate-job-conf-files/README.md
@@ -0,0 +1,13 @@
+# Job configuration generator
+
+This is a python utility for generating job configuration files with combinations of `variant` parameters (see `wikipedia_sweep.json`).
+
+## Before running
+- Create a copy of `wikipedia_sweep.json` to create your own configuration template OR modify `wikipedia_sweep.json` with your values (like file paths etc).
+
+## To generate config set do:
+```
+python3 generate_job_configuration_files.py <your configuration template (eg wikipedia_sweep.json)>
+``
+
+A folder with a name `jobs_<benchmarkID>` should be created with all the job configuration files
---
+++ utils/generate-job-conf-files/generate_job_configuration_files.py
@@ -0,0 +1,43 @@
+import json
+import itertools
+import os
+import argparse
+
+def generate_json_files(input_file):
+    with open(input_file, 'r') as f:
+        input_data = json.load(f)
+
+    invariants = input_data['invariants']
+    variants = input_data['variants']
+
+    variant_keys = list(variants.keys())
+    variant_values = list(variants.values())
+    combinations = list(itertools.product(*variant_values))
+
+    output_dir = 'jobs_' + invariants["benchmarkID"]
+    os.makedirs(output_dir, exist_ok=True)
+
+    for idx, combination in enumerate(combinations):
+        result = invariants.copy()
+
+        idv = invariants["benchmarkID"] + "_D" + str(invariants["numDocs"]) + "_FF" + str(invariants["flushFreq"])
+        for key, value in zip(variant_keys, combination):
+            result[key] = value
+            idv += "_" + key + str(value)
+
+        result["benchmarkID"] = idv
+        filename = f'config_{idv}.json'
+        filepath = os.path.join(output_dir, filename)
+        
+        with open(filepath, 'w') as json_file:
+            json.dump(result, json_file, indent=4)
+
+        print(f"Generated {filepath}")
+
+    print("A total of " + str(idx) + " job configuration files have been generated.")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate JSON files based on input configuration.")
+    parser.add_argument('input_file', help="Path to the input JSON file")
+    args = parser.parse_args()
+    generate_json_files(args.input_file)
