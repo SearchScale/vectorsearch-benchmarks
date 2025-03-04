218e6156689e33422a7746bd5bbf650e4ea3606b
Vivek Narang <vivek@searchscale.com>
2025-03-04 18:47:58 -0500

    openai parquet to csv util id range bug fix

--- utils/openai/openai_parquet_to_csv.py
+++ utils/openai/openai_parquet_to_csv.py
@@ -6,22 +6,25 @@ with open('OpenAI_5Mx1536.csv', 'w', newline='') as ds_writer:
     print("Starting the parquet to csv conversion.")
     dswriter = csv.writer(ds_writer, delimiter=',', escapechar=' ', quoting=csv.QUOTE_ALL)
     dswriter.writerow(['id', 'vector'])
+    start = 0
     for file_name in ["/data/openai/train-{:02d}-of-10.parquet".format(i) for i in range(10)]:
         print("Reading file: ", file_name)
-        df = pd.read_parquet(file_name)
 
+        df = pd.read_parquet(file_name)
         keys = df.keys()
         rows = len(df[keys[0]])
         print("Keys in the file: ", keys)
         print("Number of rows to read/write: ", rows)
-        print("Writing to the csv file")
-        for i in range(rows):
+        print("Writing to the csv file starting from id: ", start, " to: ", str(start + rows - 1))
+
+        for i in range(start, start + rows):
             r = []
             for k in keys:
-                r.append(df[k][i].tolist())
+                r.append(df[k][i].tolist() if k == 'emb' else df[k][i])
             dswriter.writerow(r)
             if i % 10000 == 0:
                 print(".", end='')
                 sys.stdout.flush()
         print(f"Data from file {file_name} is written to the csv.")
+        start += rows
     print("Done")
