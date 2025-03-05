335661307af1e310157c3a7ec5f605b3b4e44a20
Vivek Narang <vivek@searchscale.com>
2025-03-04 23:12:13 -0500

    update openai groundtruth and query script

--- utils/openai/openai_query_and_groundtruth_parquet_to_csv.py
+++ utils/openai/openai_query_and_groundtruth_parquet_to_csv.py
@@ -1,11 +1,9 @@
 import pandas as pd
-import csv
 import sys
 
-def generate(parquet_file_path: str, csv_out_file: str, col_name: str):
-    with open(csv_out_file, 'w', newline='') as ds_writer:
+def generate(parquet_file_path: str, csv_out_file: str, col_name: str, wrap_in_b: bool):
+    with open(csv_out_file, 'w', newline='') as file:
         print("Starting the parquet to csv conversion.")
-        dswriter = csv.writer(ds_writer, delimiter=',', escapechar=' ', quoting=csv.QUOTE_NONE)
 
         df = pd.read_parquet(parquet_file_path)
         keys = df.keys()
@@ -14,8 +12,11 @@ def generate(parquet_file_path: str, csv_out_file: str, col_name: str):
         print("Number of rows to read/write: ", rows)
 
         for i in range(rows):
-            x = "[" + ",".join([str(i) for i in df[col_name][i].tolist()]) + "]"
-            dswriter.writerow([x])
+            lst = df[col_name][i].tolist()
+            x = ", ".join([str(x) for x in lst])
+            if wrap_in_b:
+                x = "[" + x + "]"
+            file.write(x.strip() + "\n")
             if i % 100 == 0:
                 print(".", end='')
                 sys.stdout.flush()
@@ -23,5 +24,5 @@ def generate(parquet_file_path: str, csv_out_file: str, col_name: str):
         print("\nDone")
 
 if __name__ == "__main__":
-    generate('/data/openai/neighbors.parquet' ,'OpenAI_5Mx1536_groundtruth_1000.csv', 'neighbors_id')
-    generate('/data/openai/test.parquet' ,'OpenAI_5Mx1536_query_1000.csv', 'emb')
+    generate('/data/openai/neighbors.parquet' ,'OpenAI_5Mx1536_groundtruth_1000.csv', 'neighbors_id', False)
+    generate('/data/openai/test.parquet' ,'OpenAI_5Mx1536_query_1000.csv', 'emb', True)
